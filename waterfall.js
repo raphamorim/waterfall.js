7b62fcafb3742712cdc79f61139d1466f46b59a0
Israel Teixeira <israelst@gmail.com>
2015-11-23 19:23:49 -0200

    Replace older waterfall.

--- waterfall.js
+++ waterfall.js
@@ -9,104 +9,66 @@
    + Documentation: https://github.com/raphamorim/waterfall
 */
 
-;(function() {
-  this.defaults = {
-    container: '.waterfall',
-    items: '.item',
-  };
+function waterfall(container){
+    // Freeze the list of nodes
+    var els = [].map.call(container.children, function(el){
+        el.style.position = 'absolute';
+        return el;
+    });
+    container.style.position = 'relative';
 
-  this.init = function(sel, items) {
-    var el = document.querySelector((sel || this.defaults.container))
-    if (!el)
-      return console.log('Waterfall.js: Element doens\'t exists!')
+    function px(n){ return n + 'px';}
 
-    var childs = document.querySelectorAll((items || this.defaults.items))
-    el.style.visibility = 'hidden'
-    this._getColumnsNumber(el, childs)
-  }
-
-  this._getOuterHeight = function(el) {
-    var styles = window.getComputedStyle(el),
-        margin = parseFloat(styles['marginTop']) + 
-            parseFloat(styles['marginBottom']) +
-            parseFloat(styles['paddingTop']) + 
-            parseFloat(styles['paddingBottom']);
-    return Math.ceil(el.offsetHeight + margin);
-  }
-
-  this._getColumnsNumber = function(el, childs) {
-    var ncols = 0,
-        columnWidth = 0,
-        containerWidth = el.offsetWidth; 
-
-    for (var i = 0; i <= 10; i++) {
-      columnWidth += childs[i].offsetWidth;
-      if (columnWidth > containerWidth)
-        break;
-      ++ncols;
+    function margin(name, el){
+        var style = window.getComputedStyle(el);
+        return parseFloat(style['margin' + name]) || 0;
     }
-    
-    this._normalize(el, childs, ncols);
-  }
+    function y(el){ return parseFloat(el.style.top) ; }
+    function x(el){ return parseFloat(el.style.left); }
+    function width(el){ return el.clientWidth; }
+    function height(el){ return el.clientHeight; }
+    function bottom(el){ return y(el) + height(el) + margin('Bottom', el);}
+    function right(el){ return x(el) + width(el) + margin('Right', el);}
 
-  this._normalize = function(el, childs, grids) {
-    el.style.columnGap = '10px'
-    el.style.columnCount = grids
-    el.style.columnFill = 'balance'
-    el.style.webkitColumnGap = '10px'
-    el.style.webkitColumnCount = grids
-    el.style.webkitColumnFill = 'balance'
-    el.style.MozColumnGap = '15px'
-    el.style.MozColumnCount = grids
-    el.style.MozColumnFill = 'balance'
-
-    this._insertElements(el, childs, this._orderGrid, this._getMaxHeight, grids)
-  }
-
-  this._orderGrid = function(childs, ncols) {
-    var self = this,
-        nelms = childs.length,
-        list = [],
-        ncolsRange = self._range(ncols);
-
-    for (var i = 0; i < ncols; i++) {      
-      var columnRange = self._range(ncolsRange[i] + ncols, nelms, ncols);
-      columnRange.unshift(ncolsRange[i]);
-      list = list.concat(columnRange);
-    }
-    return list;
-  }
-
-  this._insertElements = function(el, elms, orderingFunction, getMaxHeight, ncols) {
-    var ordering = orderingFunction(elms, ncols);
-
-    el.innerHTML = '';
-    el.style.visibility = 'visible';
-    for (var i = 0; i < elms.length; i++) {
-      el.appendChild(elms[ordering[i]]);
-    }
-  }
-
-  this._range = function(start, stop, step) {
-    if (typeof stop == 'undefined') {
-      stop = start;
-      start = 0;
+    function sort(l){
+        l = l.sort(function(a, b){
+            if(bottom(a) === bottom(b)){
+                return x(b) - x(a);
+            }else{
+                return bottom(b) - bottom(a);
+            }
+        })
     }
 
-    if (typeof step == 'undefined')
-      step = 1;
+    var boundary = [];
 
-    if ((step > 0 && start >= stop) || (step < 0 && start <= stop))
-      return [];
-
-    var result = [];
-    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
-      result.push(i);
+    // Deal with the first element.
+    if(els.length){
+        els[0].style.top = '0px';
+        els[0].style.left = px(margin('Left', els[0]));
+        boundary.push(els[0]);
     }
 
-    return result;
-  };
+    // Deal with the first line.
+    for(var i = 1; i < els.length; i++){
+        var prev = els[i - 1],
+        el = els[i],
+        thereIsSpace = right(prev) + width(el) <= width(container);
+        if(!thereIsSpace) break;
+            el.style.top = prev.style.top;
+        el.style.left = px(right(prev) + margin('Left', el));
+        boundary.push(el);
+    }
 
-  if (typeof window === "object")
-    window.waterfall = this.init.bind(this)
-}());
+    // Place following elements at the bottom of the smallest column.
+    for(; i < els.length; i++){
+        sort(boundary)
+        var el = els[i],
+        minEl = boundary.pop();
+        el.style.top = px(bottom(minEl) + margin('Top', el));
+        el.style.left = px(x(minEl));
+        boundary.push(el);
+    };
+
+    container.style.height = px(bottom(boundary[0]) + margin('Bottom', boundary[0]));
+}
