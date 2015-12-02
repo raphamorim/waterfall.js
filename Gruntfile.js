c0bf4d7f4838ef853fd704003222612efe83696f
Raphael Amorim <rapha850@gmail.com>
2015-12-02 02:12:17 -0200

    fix somethings in lastest changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,24 +4,21 @@ grunt.initConfig({
    uglify: {
       my_target: {
          files: {
-            'lib/waterfall.min.js': ['lib/waterfall.js']
+            'waterfall.min.js': ['src/**/*.js']
          }
       }
-   }, // Uglify
+   },
 
    watch: {
       dist: {
-         files: ['lib/waterfall.js'], 
-	 tasks: ['uglify']
+         files: ['src/**/*.js'], 
+	      tasks: ['uglify']
       }
-   } // Watch
+   }
 });
 
-// Tasks
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-watch');
 
-// Start Watch
-grunt.registerTask('w', ['watch']);
-
+grunt.registerTask('default', ['watch']);
 module.exports = grunt;
