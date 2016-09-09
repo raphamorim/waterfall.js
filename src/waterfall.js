a7d48427eb8710e8ac7938c65f57964e23d01f19
Raphael Amorim <rapha850@gmail.com>
2016-09-09 13:37:02 -0300

    rewriting scripts

--- src/waterfall.js
+++ src/waterfall.js
@@ -19,91 +19,85 @@
     } else {
         root.waterfall = factory;
     }
-}(this, function waterfall(container) {
-    if (typeof(container) === 'string')
+}(this, function waterfall(container){
+    if(typeof(container) === 'string')
         container = document.querySelector(container);
 
-    function style(el) {
-        return window.getComputedStyle(el);
-    }
+    // Freeze the list of nodes
+    var els = [].map.call(container.children, function(el){
+        el.style.position = 'absolute';
+        return el;
+    });
+    container.style.position = 'relative';
 
-    function margin(name, el) {
-        return parseFloat(style(el)['margin' + name]) || 0;
+    function margin(name, el){
+        var style = window.getComputedStyle(el);
+        return parseFloat(style['margin' + name]) || 0;
     }
-
-    function px(n){ return parseFloat(n) + 'px'; }
+    function px(n){ return n + 'px'; }
     function y(el){ return parseFloat(el.style.top) ; }
     function x(el){ return parseFloat(el.style.left); }
-    function width(el){ return parseFloat(style(el).width); }
-    function height(el){ return parseFloat(style(el).height); }
+    function width(el){ return el.clientWidth; }
+    function height(el){ return el.clientHeight; }
     function bottom(el){ return y(el) + height(el) + margin('Bottom', el); }
     function right(el){ return x(el) + width(el) + margin('Right', el); }
 
-    function sort(l) {
-        l = l.sort(function(a, b) {
-            var bottom_diff = bottom(b) - bottom(a);
-            return bottom_diff || x(b) - x(a);
+    function sort(l){
+        l = l.sort(function(a, b){
+            if(bottom(a) === bottom(b)){
+                return x(b) - x(a);
+            }else{
+                return bottom(b) - bottom(a);
+            }
         });
     }
 
-    function Boundary(firstRow){
-        var els = firstRow;
-        sort(els);
+    var boundary = [];
 
-        this.add = function (el){
-            els.push(el);
-            sort(els);
-            els.pop();
-        };
-
-        this.min = function(){ return els[els.length - 1]; };
-        this.max = function(){ return els[0]; };
+    // Deal with the first element.
+    if(els.length){
+        els[0].style.top = '0px';
+        els[0].style.left = px(margin('Left', els[0]));
+        boundary.push(els[0]);
     }
 
-    function placeEl(el, top, left) {
-        el.style.position = 'absolute';
-        el.style.top = px(top);
-        el.style.left = px(left);
+    // Deal with the first line.
+    for(var i = 1; i < els.length; i++){
+        var prev = els[i - 1],
+        el = els[i],
+        thereIsSpace = right(prev) + width(el) <= width(container);
+        if(!thereIsSpace) break;
+            el.style.top = prev.style.top;
+        el.style.left = px(right(prev) + margin('Left', el));
+        boundary.push(el);
     }
 
-    function placeFirstElement(el){
-        placeEl(el, 0, margin('Left', el));
-    }
-
-    function placeAtTheFirstLine(prev, el){
-        placeEl(el, prev.style.top, right(prev) + margin('Left', el));
-    }
-
-    function placeAtTheSmallestColumn(minEl, el){
-        placeEl(el, bottom(minEl) + margin('Top', el), x(minEl));
-    }
-
-    function adjustContainer(container, maxEl) {
-        container.style.position = 'relative';
-        container.style.height = px(bottom(maxEl) + margin('Bottom', maxEl));
-    }
-
-    function thereIsSpace(els, i) {
-        return right(els[i - 1]) + width(els[i]) <= width(container);
-    }
-
-    var els = container.children;
-
-    if (els.length) {
-        placeFirstElement(els[0]);
-    }
-
-    for (var i = 1; i < els.length && thereIsSpace(els, i); i++) {
-        placeAtTheFirstLine(els[i - 1], els[i]);
-    }
-
-    var firstRow = [].slice.call(els, 0, i);
-    var boundary = new Boundary(firstRow);
-
+    // Place following elements at the bottom of the smallest column.
     for(; i < els.length; i++){
-        placeAtTheSmallestColumn(boundary.min(), els[i]);
-        boundary.add(els[i]);
+        sort(boundary);
+        var el = els[i],
+            minEl = boundary.pop();
+        el.style.top = px(bottom(minEl) + margin('Top', el));
+        el.style.left = px(x(minEl));
+        boundary.push(el);
     }
 
-    adjustContainer(container, boundary.max());
+    sort(boundary);
+    var maxEl = boundary[0];
+    container.style.height = px(bottom(maxEl) + margin('Bottom', maxEl));
+
+    // Responds to window resize
+    var containerWidth = width(container);
+    function resize(e) {
+        if(width(container) != containerWidth){
+            e.target.removeEventListener(e.type, arguments.callee);
+            waterfall(container);
+        }
+    }
+
+    if (window.addEventListener) {
+        window.addEventListener('resize', resize);
+    } else {
+        document.body.onresize = resize;
+    }
 }));
