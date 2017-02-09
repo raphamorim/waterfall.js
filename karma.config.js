c03f2a1c49a4e422af15dcb3d846e055702c42f4
Thiago Santos <thia.mdossantos@gmail.com>
2017-02-09 16:12:45 -0200

    add git hooks to prevent commit of code that does not follow he code style or the tests are broken and build the dist to the latest state

--- karma.config.js
+++ karma.config.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = 'test'
 module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: __dirname,
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -55,19 +55,14 @@ module.exports = function(config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+    singleRun: true,
+    autoWatch: true
   })
 }
